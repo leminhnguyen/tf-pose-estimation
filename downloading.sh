3c65d7aa82a6128ff919406305a6574f09e20d8e
leminhnguyen <leminhnguyen.bk@gmail.com>
2020-04-11 22:02:31 +0700

    updated

--- downloading.sh
+++ downloading.sh
@@ -1,3 +1,6 @@
+sudo apt-get install libllvm-7-ocaml-dev libllvm7 llvm-7 llvm-7-dev llvm-7-doc llvm-7-examples llvm-7-runtime
+export LLVM_CONFIG=/usr/bin/llvm-config-7 
+
 pip install -r requirements.txt
 pip install tensorflow==1.14
 
